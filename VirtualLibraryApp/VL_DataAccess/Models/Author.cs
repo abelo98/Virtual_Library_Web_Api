e945a86f113d81c3bfa9c50d892b667a03b5ca96
Abel <abelo08cruz@gmail.com>
2022-11-24 00:30:41 -0500

    adds author model

---
+++ VirtualLibraryApp/VL_DataAccess/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL_DataAccess.Models
+{
+    public class Author: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Nacionality { get; set; }
+    }
+}
