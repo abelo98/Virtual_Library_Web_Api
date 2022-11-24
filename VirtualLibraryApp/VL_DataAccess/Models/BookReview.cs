11d44f67d53f773a92d4f183fa157f3dd019205a
Abel <abelo08cruz@gmail.com>
2022-11-24 00:47:20 -0500

    adds rate enum and BookReview model

---
+++ VirtualLibraryApp/VL_DataAccess/Models/BookReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL_DataAccess.Models
+{
+    public class BookReview:BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public DateTime PostedDate { get; set; }
+        public string? Opinion { get; set; }
+        public RateReview Rate { get; set; }
+    }
+}
