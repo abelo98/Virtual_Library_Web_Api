9087ea626b258d92734bcb71a4175d6b9f3481c0
Abel <abelo08cruz@gmail.com>
2022-11-24 00:27:39 -0500

    adds book model

---
+++ VirtualLibraryApp/VL_DataAccess/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL_DataAccess.Models
+{
+    public class Book: BaseEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string EditorialName { get; set; }
+        public int Pages { get; set; }
+        public DateTime PublishingDate { get; set; }
+        public string DowloadUrl { get; set; }
+        public int ISBN { get; set; } /*check that int suits the number*/
+        public int Rate{ get; set; }
+
+    }
+}
