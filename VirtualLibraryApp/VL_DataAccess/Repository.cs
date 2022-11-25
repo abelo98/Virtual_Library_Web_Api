b65111c3dfb8e76d97845e22e72e6bce831c5240
Abel <abelo08cruz@gmail.com>
2022-11-25 14:58:23 -0500

    repository generic

--- VirtualLibraryApp/VL_DataAccess/Repository.cs
+++ VirtualLibraryApp/VL_DataAccess/Repository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VL_DataAccess.Models;
 
 namespace VL_DataAccess
 {
-    public class Repository
+    public class Repository<TEntity> where TEntity : IBaseEntity
     {
     }
 }
