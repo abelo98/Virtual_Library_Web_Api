a6c6cbf7869212d6c13fdae704cc1a21684ab434
Abel <abelo08cruz@gmail.com>
2022-11-26 23:50:34 -0500

    adds AuthorController and AuhtorDto

---
+++ VirtualLibraryApp/VL_DataManager/Controllers/AuthorsController.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Services_Layer;
+using VL_DataAccess.Models;
+using VL_DataManager.Dtos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VL_DataManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        readonly IAuthorService _authorService;
+        readonly IMapper _mapper;
+
+        public AuthorsController(IAuthorService authorService, IMapper mapper)
+        {
+            _authorService = authorService;
+            _mapper = mapper;
+        }
+
+        // GET: api/<AuthorController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthorController>/5
+        [HttpGet("{userId}")]
+        public string Get(Guid userId)
+        {
+            return "value";
+        }
+
+        // POST api/<AuthorController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AuthorDto authorDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Author author = _mapper.Map<Author>(authorDto);
+                await _authorService.Insert(author);
+                return Ok(authorDto);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Error inserting data on the database");
+            }
+
+
+        }
+
+        // PUT api/<AuthorController>/5
+        [HttpPut("{userId}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpPatch("{userId}")]
+        public async Task<IActionResult> Patch(Guid id, [FromBody] JsonPatchDocument<AuthorDto> authorDto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var author = _mapper.Map<JsonPatchDocument<Author>>(authorDto);
+                Author updatedEmployee = await _authorService.PartialUpdate(id, author);
+                var response = _mapper.Map<AuthorDto>(updatedEmployee);
+
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Error inserting data on the database");
+            }
+        }
+
+        // DELETE api/<AuthorController>/5
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteAsync(Guid userId)
+        {
+            try
+            {
+                await _authorService.Delete(userId);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Error deleting data from the database");
+            }
+        }
+    }
+}
---
+++ VirtualLibraryApp/VL_DataManager/Dtos/AuthorDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VL_DataManager.Dtos
+{
+    public class AuthorDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nationality { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime BirthDate { get; set; }
+    }
+}
