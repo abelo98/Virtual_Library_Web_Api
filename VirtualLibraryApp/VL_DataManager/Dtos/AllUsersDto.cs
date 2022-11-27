b77272231e0a06d0f6375b336a71e4378c4fc5cc
Abel <abelo08cruz@gmail.com>
2022-11-27 17:48:54 -0500

    code fot AllUserDto

---
+++ VirtualLibraryApp/VL_DataManager/Dtos/AllUsersDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VL_DataManager.Dtos
+{
+    public class AllUsersDto
+    {
+        public int TotalUsers { get { return Users.Count; } set { } }
+        public ICollection<LibraryUser> Users { get; set; }
+        public int TotalAuthors { get; set; }
+    }
+}
