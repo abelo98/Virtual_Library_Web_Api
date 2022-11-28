94bbf4c1a6720c28aba4e688b2512aa915800c1b
Abel <abelo08cruz@gmail.com>
2022-11-28 01:45:48 -0500

    adds authorDetailsServiceMOdel

---
+++ VirtualLibraryApp/Services_Layer/Models/AuthorDetailsServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_Layer.Models
+{
+    public class AuthorDetailsServiceModel
+    {
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Subscribers { get; set; }
+        public ICollection<BookServiceModel> Books { get; set; }
+    }
+}
